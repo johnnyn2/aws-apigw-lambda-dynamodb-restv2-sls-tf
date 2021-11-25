c6f8a44d2a2526c223bc735bf8015b7d78a47fbd
Ho Wai Kin Johnny <johnnyhohohohohoho@gmail.com>
2021-11-25 10:50:29 +0800

    chore: create table

--- createTable.js
+++ createTable.js
@@ -1,3 +1,6 @@
+/**
+ * This script is only used for setting up local dynamodb. You should deploy your database on cloud and connect it for usage.
+ */
 const { DynamoDB } = require('aws-sdk');
 const config = require('./config.json');
 
