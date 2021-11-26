2ac344bc6325f2b9794cf8b4a6cde4be76e37244
Ho Wai Kin Johnny <johnnyhohohohohoho@gmail.com>
2021-11-26 12:43:09 +0800

    chore: edit create/remove layer module script to pass inline args to scripts

--- createLayerModule.js
+++ createLayerModule.js
@@ -1,24 +1,9 @@
-const readline = require('readline');
 const fs = require('fs');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const config = require('./config.json');
 
 const { defaultLambdaLayerSrcPath } = config;
-
-function askQuestion(query) {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-
-    return new Promise((resolve) =>
-        rl.question(query, (ans) => {
-            rl.close();
-            resolve(ans);
-        }),
-    );
-}
 
 const tplFn = `function example() {}
 
@@ -29,34 +14,30 @@ module.exports = {
 
 (async function () {
     try {
-        console.log(
-            'Default lambda layer source path: ',
-            defaultLambdaLayerSrcPath,
-        );
-        const layerPath = await askQuestion(
-            '1. What is the module path? E.g. service/user-service\n',
-        );
-        const layer = `${defaultLambdaLayerSrcPath}${layerPath}`;
-        if (fs.existsSync(layer)) {
-            throw new Error('Module already exists!');
-        } else {
-            await fs.promises.mkdir(layer, { recursive: true });
-            const index = `${layer}/index.js`;
-            await fs.promises.writeFile(index, tplFn);
-            console.log('\nInitializing module...');
-            await exec('npm init -y', { cwd: layer });
-            console.log('Done!');
-            console.log('Installing your module into node_modules...');
-            await exec(`npm install file:${layer}`);
-            const moduleName = layerPath.split('/');
-            console.log(
-                `Done! package.json added ${
-                    moduleName[moduleName.length - 1]
-                } dependency`,
-            );
-            console.log(
-                `\nYou can write your module code in ${index} now. Remember to export data from your module.\n`,
-            );
+        for (let i = 2; i < process.argv.length; i++) {
+            const layerPath = process.argv[i];
+            const layer = `${defaultLambdaLayerSrcPath}${layerPath}`;
+            if (fs.existsSync(layer)) {
+                throw new Error('Module already exists!');
+            } else {
+                await fs.promises.mkdir(layer, { recursive: true });
+                const index = `${layer}/index.js`;
+                await fs.promises.writeFile(index, tplFn);
+                console.log('Initializing module...');
+                await exec('npm init -y', { cwd: layer });
+                console.log('Done!');
+                console.log('Installing your module into node_modules...');
+                await exec(`npm install file:${layer}`);
+                const moduleName = layerPath.split('/');
+                console.log(
+                    `Done! package.json added ${
+                        moduleName[moduleName.length - 1]
+                    } dependency`,
+                );
+                console.log(
+                    `\nYou can write your module code in ${index} now. Remember to export data from your module.\n`,
+                );
+            }
         }
     } catch (e) {
         console.error(e);
--- removeLayerModule.js
+++ removeLayerModule.js
@@ -1,4 +1,3 @@
-const readline = require('readline');
 const fs = require('fs');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
@@ -6,43 +5,25 @@ const config = require('./config.json');
 
 const { defaultLambdaLayerSrcPath } = config;
 
-function askQuestion(query) {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-
-    return new Promise((resolve) =>
-        rl.question(query, (ans) => {
-            rl.close();
-            resolve(ans);
-        }),
-    );
-}
-
 (async function () {
     try {
-        console.log(
-            'Default lambda layer source path: ',
-            defaultLambdaLayerSrcPath,
-        );
-        const layerPath = await askQuestion(
-            '1. What is the module path? E.g. service/user-service\n',
-        );
-        const layer = `${defaultLambdaLayerSrcPath}${layerPath}`;
-        if (fs.existsSync(layer)) {
-            console.log('\nRemoving module...');
-            await fs.promises.rm(layer, { recursive: true });
-            console.log('Done!');
-            const modulePath = layerPath.split('/');
-            console.log('Uninstalling module from node_modules');
-            const dependency = modulePath[modulePath.length - 1];
-            await exec(`npm uninstall ${dependency}`);
-            console.log(
-                `Done! package.json removed ${dependency} dependency\n`,
-            );
-        } else {
-            throw new Error('Module does not exist!');
+        for (let i = 2; i < process.argv.length; i++) {
+            const layerPath = process.argv[i];
+            const layer = `${defaultLambdaLayerSrcPath}${layerPath}`;
+            if (fs.existsSync(layer)) {
+                console.log('Removing module...');
+                await fs.promises.rm(layer, { recursive: true });
+                console.log('Done!');
+                const modulePath = layerPath.split('/');
+                console.log('Uninstalling module from node_modules');
+                const dependency = modulePath[modulePath.length - 1];
+                await exec(`npm uninstall ${dependency}`);
+                console.log(
+                    `Done! package.json removed ${dependency} dependency\n`,
+                );
+            } else {
+                throw new Error('Module does not exist!');
+            }
         }
     } catch (e) {
         console.error(e);
