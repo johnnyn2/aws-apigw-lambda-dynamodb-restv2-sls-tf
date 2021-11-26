72291ff0c1000d0ef7f2000326e98fd0ad1f6132
Ho Wai Kin Johnny <johnnyhohohohohoho@gmail.com>
2021-11-26 16:53:59 +0800

    chore: create table script aws config

--- createTable.js
+++ createTable.js
@@ -1,13 +1,15 @@
 /**
  * This script is only used for setting up local dynamodb. You should deploy your database on cloud and connect it for usage.
  */
-const { DynamoDB } = require('aws-sdk');
+const AWS = require('aws-sdk');
 const config = require('./config.json');
 
-const db = new DynamoDB({
-    region: 'local-env',
+AWS.config.update({
+    region: 'ap-east-1',
     endpoint: 'http://localhost:8000',
 });
+
+const db = new AWS.DynamoDB();
 
 (async function () {
     const table = config.table;
